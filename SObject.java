2f0f27ef2e02c77a6fb1159589dfbf04e5c4cfbe
Ramsey Tomasi-Carr <ramsey.tomasi.carr@gmail.com>
2013-05-15 20:50:07 -0400

    Strange forces at work

--- SObject.java
+++ SObject.java
@@ -5,70 +5,62 @@ import java.util.ArrayList;
 import acm.graphics.GOval;
 
 public abstract class SObject extends GOval{
-
+	
 	/**
 	*Abstract superclass for celestial bodies
 	*/
 
 	//Instance Variables
-	
 	private double mass;
 	private double radius;
-
-	
 	private double[] velocity;
-
 	private ArrayList<double[]> forces;
 
 	//Constructor
-	
 	public SObject(double[] pos, double radius, double mass){
 	super(radius*2, radius*2);
 	setPosition(pos[0], pos[1]);
 	velocity = new double[] {0, 0};
 	mass = mass;
 	}
-	
-	
+
 	//Methods get
 	
 	public double[] getVel() {
 		return velocity;
-	}
-	
+	}	
 	public double[] getPos() {
 		return position;
 	}
-	
 	public double getRadius() {
 		return radius;
 	}
-	
 	public double getMass() {
 		return mass;
 	}
-
 	public ArrayList<double[]> getForces(){
 		return forces;
 	}
-	
 	
 	//Methods Set
 	
 	public void setVel(double[] vel) {
 		velocity = vel;
 	}
-	
 	public void setPos(double[] newPos) {
 		setPosition(pos[0], pos[1]);
 	}
-	
 	public void setMass(double m){
 		if(m != 0){mass = m;}
 	}
-	
 	public void setRadius(double r){
 		if(r != 0){radius = r;}
+	}
+	
+	//Methods: act, etc.
+	
+	public void act() {
+		//code to be added
 	}
 	
 }
